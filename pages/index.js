2695da104c0a001f65476ea1a1abddc3cbfd7706
Rider Jensen <riderjensen@gmail.com>
2019-04-02 11:13:45 -0600

    add class component with initprops lifecycle

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 
-export default function index() {
-	return (
-		<div>
-			<h1>The main page</h1>
-			<p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-		</div>
-	)
+export default class index extends Component {
+	static async getInitialProps(context) {
+		console.log(context);
+		// await for a promise to resolve
+		return { appName: 'New app' };
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>The main page of {this.props.appName}</h1>
+				<p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+			</div>
+		)
+	}
 }
