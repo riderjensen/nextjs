0497cad92d525fa9375298921d4ba1dea2722b27
Rider Jensen <riderjensen@gmail.com>
2019-04-02 11:03:45 -0600

    Add user component and show it

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Router from 'next/router';
 
+import User from '../../components/User';
+
 export default function index() {
 	return (
 		<div>
 			<h1>The auth page</h1>
+			<User name="Rider" age={23} />
 			<button onClick={() => Router.push('/')}>Go to home</button>
 		</div>
 	)
