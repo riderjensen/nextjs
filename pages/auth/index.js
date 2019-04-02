5d342572e437594f46e42b5666e5452254b3a579
Rider Jensen <riderjensen@gmail.com>
2019-04-02 11:00:49 -0600

    Add navigation between pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Router from 'next/router';
 
 export default function index() {
 	return (
 		<div>
 			<h1>The auth page</h1>
+			<button onClick={() => Router.push('/')}>Go to home</button>
 		</div>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 export default function index() {
 	return (
 		<div>
 			<h1>The main page</h1>
+			<p>Go to <Link href="/auth"><a>Auth</a></Link></p>
 		</div>
 	)
 }
