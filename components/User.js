cbb114a40cdc4c40ab18a1f677f2fc1e8643ed21
Rider Jensen <riderjensen@gmail.com>
2019-04-02 11:06:29 -0600

    adding styles

--- components/User.js
+++ components/User.js
@@ -5,6 +5,12 @@ export default function User(props) {
 		<div>
 			<h1>{props.name}</h1>
 			<p>Age: {props.age}</p>
+			<style jsx>{`
+				div {
+					border: 1px solid #eee;
+					text-align: center;
+				}
+			`}</style>
 		</div>
 	)
 }
